8e732bf75afea68b96adc3d52b36e345a01c861c
Aaditya Gupta <aadityagupta400@gmail.com>
2024-12-26 12:34:36 +0530

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-zinc-700'>
+        Welcome to White Elephant
+        </div>
+    </>
+  )
+}
+
+export default App
